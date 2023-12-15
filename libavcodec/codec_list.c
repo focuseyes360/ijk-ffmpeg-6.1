0bb1c33872e8f3e8656677bf6ceeda38e781ddfc
mahongyin <1976222027@qq.com>
2023-12-15 16:33:36 +0800

    ijk-ffmpeg6.1

---
+++ libavcodec/codec_list.c
@@ -0,0 +1,13 @@
+static const FFCodec * const codec_list[] = {
+    &ff_aac_encoder,
+    &ff_libx264_encoder,
+    &ff_libx265_encoder,
+    &ff_bmp_decoder,
+    &ff_h264_decoder,
+    &ff_hevc_decoder,
+    &ff_mpeg2video_decoder,
+    &ff_vc1_decoder,
+    &ff_vp9_decoder,
+    &ff_aac_decoder,
+    &ff_av1_decoder,
+    NULL };
